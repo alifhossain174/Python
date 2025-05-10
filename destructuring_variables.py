d2a626cb2580b363b07e3366c811700b5814e6ed
Fahim <alifhossain174@gmail.com>
2025-05-10 19:14:27 +0200

    Updating Code

---
+++ destructuring_variables.py
@@ -0,0 +1,6 @@
+myDictionary = {"name": "Fahim", "age": 28, "contact": "01969005035"}
+
+print(list(myDictionary.items()))
+
+for key, value in myDictionary.items() :
+    print(f"Key is {key} and value is {value}")
